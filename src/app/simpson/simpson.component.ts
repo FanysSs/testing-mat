cc5c5b7cf1a0ceb3313a0beee3e92867647abdeb
FanysSs <estefania.rguezb21@gmail.com>
2024-10-24 08:08:22 -0600

    git branch simpson

---
+++ src/app/simpson/simpson.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simpson',
+  standalone: true,
+  imports: [],
+  templateUrl: './simpson.component.html',
+  styleUrl: './simpson.component.css'
+})
+
+export class SimpsonComponent {
+  simpson(
+    f: (x: number) => number,
+    x0: number,
+    x1: number,
+    num_seg: number,
+  ): number {
+    const h = (x1 - x0) / num_seg;
+    let sum = f(x0) + f(x1);
+    for (let i = 1; i < num_seg; i++) {
+      const x = x0 + i * h;
+      sum += (i % 2 === 0 ? 2 : 4) * f(x);
+    }
+    return (h / 3) * sum;
+  }
+}
+

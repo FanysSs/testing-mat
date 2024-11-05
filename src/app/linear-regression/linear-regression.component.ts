bf14e795c0b371d0c1b206df291233c02d839f9d
FanysSs <estefania.rguezb21@gmail.com>
2024-11-05 11:31:35 -0600

    menu update

--- src/app/linear-regression/linear-regression.component.ts
+++ src/app/linear-regression/linear-regression.component.ts
@@ -13,9 +13,11 @@ import { FormsModule } from '@angular/forms';
 export class LinearRegressionComponent {
   xInput: string = '';
   yInput: string = '';
+  predictX: number | null = null;
   slope: number | null = null;
   intercept: number | null = null;
   resultado: string | null = null;
+  prediccion: string | null = null;
 
   calcular(): void {
     const x = this.xInput.split(',').map(num => parseFloat(num.trim()));
@@ -29,6 +31,15 @@ export class LinearRegressionComponent {
     const { slope, intercept } = linearRegression(x, y);
     this.slope = slope;
     this.intercept = intercept;
-    this.resultado = `La pendiente (slope) es: ${this.slope.toFixed(5)}, y la intersección (intercept) es: ${this.intercept.toFixed(5)}`;
+    this.resultado = `La pendiente es: ${this.slope.toFixed(5)}, y la intersección (intercept) es: ${this.intercept.toFixed(5)}`
+  }
+
+  predecir(): void {
+    if (this.slope !== null && this.intercept !== null && this.predictX !== null) {
+      const predictedY = this.slope * this.predictX + this.intercept;
+      this.prediccion = `Para x = ${this.predictX}, el valor predicho de y es: ${predictedY.toFixed(5)}`;
+    } else {
+      this.prediccion = 'Por favor, realice el cálculo de la regresión antes de predecir.';
+    }
   }
 }
